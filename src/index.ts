9c4b4ff5436b533b5ab15114455ea6b752688e0d
Glavin Wiechert <glavin.wiechert@gmail.com>
2016-09-03 22:50:06 -0300

    Add newUnibeautify method for setting up Unibeautify instance
    Useful for testing

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,11 @@ export * from "./options";
 import {Unibeautify} from "./beautifier";
 import {Options} from "./options";
 import {Languages} from "./languages";
-const unibeautify = new Unibeautify();
-unibeautify.loadOptions(Options);
-unibeautify.loadLanguages(Languages);
-export default unibeautify;
+
+export function newUnibeautify(): Unibeautify {
+  const unibeautify = new Unibeautify();
+  unibeautify.loadOptions(Options);
+  unibeautify.loadLanguages(Languages);
+  return unibeautify;
+}
+export default newUnibeautify();
